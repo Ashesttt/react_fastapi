4c3246d0f3c1464003cab9fed9f8300b4422f1da
Huang Jihao <705626108@qq.com>
2024-02-13 11:55:24 +0800

    loaclhost

--- react-fastapi/main.py
+++ react-fastapi/main.py
@@ -12,6 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://localhost:8000",
+    "http://127.0.0.1:8000/cfw"
     "http://47.92.99.199:3000",
     "http://47.92.99.199:30"
 ]
