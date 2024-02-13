de5b5b9b9fd66b805c079fe1fd6da42d3e9d2b9d
Huang Jihao <705626108@qq.com>
2024-02-13 12:06:15 +0800

    loaclhost

--- react-fastapi/src/App.js
+++ react-fastapi/src/App.js
@@ -122,7 +122,7 @@ function App() {
                     </div>
                 </header>
                 <Routes>
-                    <Route path="*" element={window.location.pathname.startsWith('/api') ? null : <ConfigWeb />} />
+                    {/*<Route path="*" element={window.location.pathname.startsWith('/api') ? null : <ConfigWeb />} />*/}
                     <Route path="/ConfigWeb" element={<ConfigWeb />} />
                     <Route path="/api/wechatter" element={<WeChatter />} />
                     <Route path="/wx-bot-webhook" element={<WxBotWebhook />} />
